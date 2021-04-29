9ae84b569bc4a9552d352937646300c7b5156dfc
ArvinRad <radanarvin@gmail.com>
2021-04-29 08:04:05 +0000

    Parametarize the query scripts

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -22,12 +22,13 @@ FROM assistance_requests
 JOIN teachers ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
+WHERE cohorts.name LIKE $1
 ORDER BY teachers.name;
-`)
+`, [`%${process.argv[2]}%`])
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.teacher} has assisted in the ${user.cohort} cohort`);
   })
 })
 .catch(err => console.error('query error', err.stack));
+pool.end();
