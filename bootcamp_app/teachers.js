fcfafebabee4b8c2898d7fe8b25f2f636d324e7f
ArvinRad <radanarvin@gmail.com>
2021-04-29 07:44:07 +0000

    Create the JS file for quering the database for this app

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -17,15 +17,17 @@ pool.on('error', (err, client) => {
 
 // promise - checkout a client
 pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM assistance_requests
+JOIN teachers ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
 WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
+ORDER BY teachers.name;
 `)
 .then(res => {
   res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+    console.log(`${user.teacher} has assisted in the ${user.cohort} cohort`);
   })
 })
 .catch(err => console.error('query error', err.stack));
